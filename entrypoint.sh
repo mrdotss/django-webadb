acba296286702801b2280c2c2278bdf402954185
Mayer Reflino <mayer@fatechid.com>
2025-05-20 09:36:15 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 echo "Migrating…"
-python manage.py showmigrations
-python manage.py migrate
+python manage.py migrate --noinput
 
 echo "Collecting static files…"
 python manage.py collectstatic --noinput
 
-echo "Starting Gunicorn…"
-exec "$@"
+echo "Starting Gunicorn on port ${PORT:-8000}…"
+exec gunicorn core.wsgi:application \
+  --bind 0.0.0.0:${PORT:-8000} \
+  --workers 3
